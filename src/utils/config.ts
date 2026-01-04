12d4cd75427c1c7b07201c5bf1d0896203c83241
John Mason <johnmsn22@gmail.com>
2026-01-04 15:16:01 +0100

    chore: setup pwa

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   SITE_NAME: process.env.NEXT_PUBLIC_WEBSITE_NAME || "Church Management System",
   SITE_DESCRIPTION: process.env.NEXT_PUBLIC_WEBSITE_DESCRIPTION || "A church management and learning platform",
-  SITE_URL: process.env.NEXT_PUBLIC_WEBSITE_URL || "https://",
+  SITE_URL: process.env.NEXT_PUBLIC_WEBSITE_URL || "https://dc-church-web.vercel.app",
   BRANCH_NAME: process.env.NEXT_PUBLIC_BRANCH_NAME || "Orazi Church",
 };
